315b1e061b2127de627c34608a247649337abc86
dominikhustava <dominikhustava1@gmail.com>
2022-11-23 14:12:16 +0100

    added date sorting support for team matches

--- ipl-dashboard/src/main/java/io/hustava/ipldashboard/repository/MatchRepository.java
+++ ipl-dashboard/src/main/java/io/hustava/ipldashboard/repository/MatchRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface MatchRepository extends CrudRepository<Match, Long> {
-    List<Match> getByTeam1OrTeam2(String teamName1, String teamName2);
+    List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2);
 }
