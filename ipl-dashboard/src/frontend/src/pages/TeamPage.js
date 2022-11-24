ebc19f8852bf4272beff744ec215a89f677172ae
dominikhustava <dominikhustava1@gmail.com>
2022-11-24 13:24:27 +0100

    useParams of react router dom to pass teamName as path variable. (change single quotes to backtics in fetch)

--- ipl-dashboard/src/frontend/src/pages/TeamPage.js
+++ ipl-dashboard/src/frontend/src/pages/TeamPage.js
@@ -1,15 +1,16 @@
-import { React, useEffect, useState } from 'react'
-import { MatchDetailCard } from '../components/MatchDetailCard'
-import { MatchSmallCard } from '../components/MatchSmallCard'
+import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { MatchDetailCard } from '../components/MatchDetailCard';
+import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const TeamPage = () => {
 
   const [team, setTeam] = useState({matches: []});
-
+  const { teamName } = useParams();
   useEffect(
     () => {
         const fetchMatches = async () => {
-            const response = await fetch('http://localhost:8080/team/Kings%20XI%20Punjab');
+            const response = await fetch(`http://localhost:8080/team/${teamName}`);
             const data = await response.json();
             console.log(data);
             setTeam(data);
