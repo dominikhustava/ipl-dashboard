a90c869555f140c9540bfceb2e3db5d420dc9f87
dominikhustava <dominikhustava1@gmail.com>
2022-11-24 11:25:46 +0100

    rendering MatchSmallCard as many times as matches in fetch response saved in state

--- ipl-dashboard/src/frontend/src/pages/TeamPage.js
+++ ipl-dashboard/src/frontend/src/pages/TeamPage.js
@@ -11,6 +11,7 @@ export const TeamPage = () => {
         const fetchMatches = async () => {
             const response = await fetch('http://localhost:8080/team/Kings%20XI%20Punjab');
             const data = await response.json();
+            console.log(data);
             setTeam(data);
         };
         fetchMatches();
@@ -21,9 +22,7 @@ export const TeamPage = () => {
     <div className="TeamPage">
       <h1>{team.teamName}</h1>
       <MatchDetailCard />
-      <MatchSmallCard />
-      <MatchSmallCard />
-      <MatchSmallCard />
+      {team.matches.map(match => <MatchSmallCard />)}
     </div>
   );
 }
