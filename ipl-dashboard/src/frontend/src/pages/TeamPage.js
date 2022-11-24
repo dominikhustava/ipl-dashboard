41a1536e943ab09f798bb00e9ebb27cc3e56715c
dominikhustava <dominikhustava1@gmail.com>
2022-11-24 10:48:35 +0100

    new components for match cards, useEffect with fetch api to call our ipl-dash rest api

--- ipl-dashboard/src/frontend/src/pages/TeamPage.js
+++ ipl-dashboard/src/frontend/src/pages/TeamPage.js
@@ -1,10 +1,27 @@
-import { React } from 'react'
+import { React, useEffect } from 'react'
+import { MatchDetailCard } from '../components/MatchDetailCard'
+import { MatchSmallCard } from '../components/MatchSmallCard'
 
 export const TeamPage = () => {
+
+  useEffect(
+    () => {
+        const fetchMatches = async () => {
+            const response = await fetch('http://localhost:8080/team/Kings%20XI%20Punjab');
+            const data = await response.json();
+            console.log(data);
+        };
+        fetchMatches();
+    }
+  );
+
   return (
     <div className="TeamPage">
       <h1>Team Name</h1>
-      <h3>Latest Matches</h3>
+      <MatchDetailCard />
+      <MatchSmallCard />
+      <MatchSmallCard />
+      <MatchSmallCard />
     </div>
   );
 }
--- ipl-dashboard/src/main/java/io/hustava/ipldashboard/controller/TeamController.java
+++ ipl-dashboard/src/main/java/io/hustava/ipldashboard/controller/TeamController.java
@@ -3,11 +3,13 @@ package io.hustava.ipldashboard.controller;
 import io.hustava.ipldashboard.model.Team;
 import io.hustava.ipldashboard.repository.MatchRepository;
 import io.hustava.ipldashboard.repository.TeamRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
