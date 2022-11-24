21795cd4710351782ec932fecc77dc763555a65d
dominikhustava <dominikhustava1@gmail.com>
2022-11-24 11:11:03 +0100

    fetch api and useEffect to populate useState team, then using its property dynamicaly in html

--- ipl-dashboard/src/frontend/src/pages/TeamPage.js
+++ ipl-dashboard/src/frontend/src/pages/TeamPage.js
@@ -1,15 +1,17 @@
-import { React, useEffect } from 'react'
+import { React, useEffect, useState } from 'react'
 import { MatchDetailCard } from '../components/MatchDetailCard'
 import { MatchSmallCard } from '../components/MatchSmallCard'
 
 export const TeamPage = () => {
+
+  const [team, setTeam] = useState();
 
   useEffect(
     () => {
         const fetchMatches = async () => {
             const response = await fetch('http://localhost:8080/team/Kings%20XI%20Punjab');
             const data = await response.json();
-            console.log(data);
+            setTeam(data);
         };
         fetchMatches();
     }
@@ -17,7 +19,7 @@ export const TeamPage = () => {
 
   return (
     <div className="TeamPage">
-      <h1>Team Name</h1>
+      <h1>{team.teamName}</h1>
       <MatchDetailCard />
       <MatchSmallCard />
       <MatchSmallCard />
