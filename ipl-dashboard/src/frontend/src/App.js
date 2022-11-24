1147eac89b37480ab47eadd001c3bb185b135d88
dominikhustava <dominikhustava1@gmail.com>
2022-11-24 12:58:46 +0100

    adding react router by npm install react-router-dom. Importing it in app.js and suroundig by it <TeamPage /> component, thus making it routable

--- ipl-dashboard/src/frontend/src/App.js
+++ ipl-dashboard/src/frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 
 function App() {
   return (
     <div className="App">
-      <TeamPage />
+      <Router>
+        <TeamPage />
+      </Router>
     </div>
   );
 }
