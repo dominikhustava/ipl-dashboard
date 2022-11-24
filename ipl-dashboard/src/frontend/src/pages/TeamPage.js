64ee18b9c8024a35111e5fd3ee2ec4008348dadc
dominikhustava <dominikhustava1@gmail.com>
2022-11-24 13:33:58 +0100

    passing teamName to the MatchDetailCard and calculating there the otherTeam for cleaner desing

--- ipl-dashboard/src/frontend/src/pages/TeamPage.js
+++ ipl-dashboard/src/frontend/src/pages/TeamPage.js
@@ -22,7 +22,7 @@ export const TeamPage = () => {
   return (
     <div className="TeamPage">
       <h1>{team.teamName}</h1>
-      <MatchDetailCard match={team.matches[0]}/>
+      <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
       {team.matches.slice(1).map(match => <MatchSmallCard match={match} />)}
     </div>
   );
